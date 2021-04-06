0005c4f6a633a3de2036ede58e2801b4bcecbfef
themewiz <61708713+themewiz@users.noreply.github.com>
2021-04-06 11:06:11 -0500

    add files

---
+++ finance-main.js
@@ -0,0 +1,64 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.client_slider').slick({
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: '.prev_button',
+		nextArrow: '.next_button',
+		autoplay: true,
+		speed: 1000,
+		pauseOnHover: false,
+		responsive: [
+		{
+			breakpoint: 993,
+			settings: {
+			slidesToShow: 1,
+       
+		}		
+	},
+	]	
+	});
+	
+});
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Preload all the images in the page
+imagesLoaded({background: true}, () => document.body.classList.remove('loading'));
