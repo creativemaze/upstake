e95f2895a176940c7f1a3439f4bc21bafa9b062c
themewiz <61708713+themewiz@users.noreply.github.com>
2021-05-24 16:01:18 -0500

    add files

---
+++ law-firm-main.js
@@ -0,0 +1,64 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.slickslide').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+		{
+			breakpoint: 601,
+			settings: {
+			slidesToShow: 1,
+			dots: true,
+			fade: true
+		}
+						
+	},
+	]
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.square_slider').slick({
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		fade: true,
+		prevArrow: '.prev_slide',
+		nextArrow: '.next_slide',
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		dots: false,
+		
+	});
+	
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Preload all the images in the page
+imagesLoaded({background: true}, () => document.body.classList.remove('loading'));
