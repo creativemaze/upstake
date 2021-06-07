a98192500aca50dd6aaa07749cd2d70827bbe698
themewiz <61708713+themewiz@users.noreply.github.com>
2021-06-07 14:24:49 -0500

    add files

---
+++ mobile-app-main.js
@@ -0,0 +1,78 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.mobile_slider').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: false,
+		fade: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,		
+	});
+	
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.client-slider').slick({
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: '.prev_slide',
+		nextArrow: '.next_slide',
+		arrows: true,
+		autoplay: false,
+		speed: 1000,
+		pauseOnHover: false,
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 2,
+       
+				}	
+			},
+		
+			{
+				breakpoint: 801,
+				settings: {
+					slidesToShow: 1,
+       
+				}	
+			},
+		]	
+	});
+});
+
+//Counter script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Preload all the images in the page
+imagesLoaded({background: true}, () => document.body.classList.remove('loading'));
