d0bc80111be9927f0ca30eb6030cf24b35734162
themewiz <61708713+themewiz@users.noreply.github.com>
2021-06-21 15:00:26 -0500

    update file

--- saas-main.js
+++ saas-main.js
@@ -1,4 +1,120 @@
 "use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.saas_slider').slick({
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: false,
+		fade: true,
+		adaptiveHeight: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,		
+	});
+	
+	$('.saas_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.heading_group > div.active').removeClass('active');
+			$('.heading_group > div').eq(nextSlide).addClass('active');
+	});
+  
+	$('.heading_group > div').on('click', function(e){
+		e.preventDefault();
+		$('.heading_group > div.active').removeClass('active');
+		$(this).addClass('active');
+		var targetSlide = $(this).data('target');
+		$('.saas_slider').slick('slickGoTo', targetSlide );
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.bg_slider').slick({
+		dots: false,
+		prevArrow: '.prev_nav',
+		nextArrow: '.next_nav',
+		autoplay: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		speed: 1000,
+		autoplaySpeed: 1000,
+		pauseOnHover: false,	
+	});
+	
+});
+
+//Slick Slider (3) Three
+$(document).ready(function(){
+	$('.client-slider').slick({
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: false,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: 
+				{
+					slidesToShow: 2,
+       
+				}	
+			},
+			
+			{
+				breakpoint: 601,
+				settings: 
+				{
+					slidesToShow: 1,
+       
+				}	
+			},
+		]	
+	});
+	
+});
+
+//Animated Progress Bar
+$(document).ready(function($) {
+  function animateElements() {
+	$('.progressbar').each(function() {
+		var elementPos = $(this).offset().top;
+		var topOfWindow = $(window).scrollTop();
+		var percent = $(this).find('.circle').attr('data-percent');
+		var percentage = parseInt(percent, 10) / parseInt(100, 10);
+		var animate = $(this).data('animate');
+		if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+			$(this).data('animate', true);
+			$(this).find('.circle').circleProgress({
+			startAngle: -Math.PI / 2,
+			value: percent / 100,
+			thickness: 14,
+			size: 125,
+			animation: {
+				duration: 2800,
+			},
+			
+			fill: {
+				color: '#159957'
+			}
+			
+			}).on('circle-animation-progress', function(event, progress, stepValue) {
+				$(this).find('div').text((stepValue * 100).toFixed(1) + "%");
+			}).stop();
+		}
+    });
+}
+
+// Show animated elements
+animateElements();
+	$(window).scroll(animateElements);
+});
+
 //Animate on Scroll default settings
 $(window).on('load', function () {
 	AOS.refresh();
@@ -6,7 +122,6 @@ $(window).on('load', function () {
 
 $(function () {
 	AOS.init({
-  
 		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
 		offset: 200, // offset (in px) from the original trigger point
 		delay: 300, // values from 0 to 3000, with step 50ms
@@ -19,21 +134,5 @@ $(function () {
 	});
 });	
 
-//Counter script
-jQuery(document).ready(function($) {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-});
-
 //Preload all the images in the page
-imagesLoaded(document.querySelectorAll('.blog_box'), {background: true}, () => document.body.classList.remove('loading'));
-
-//Browser variable check
-document.documentElement.className = "js";
-document.body.classList.add('loading');
-var supportsCssVars = function() { var e, t = document.createElement("style"); 
-return t.innerHTML = "root: { --tmp-var: bold; }", document.head.appendChild(t), e = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)")), t.parentNode.removeChild(t), e };
-supportsCssVars() || alert("Please view this demo in a modern browser that supports CSS Variables.");
-
+imagesLoaded({background: true}, () => document.body.classList.remove('loading'));
