2fcc864f23744586d81c8eab3b8860ca8e86febf
themewiz <61708713+themewiz@users.noreply.github.com>
2021-07-07 14:35:04 -0500

    update file

--- gym-main.js
+++ gym-main.js
@@ -1,4 +1,5 @@
 "use strict";
+//Slick Slider (1) One
 $(document).ready(function(){
 	$('.gym_slider').slick({
 		dots: false,
@@ -27,28 +28,12 @@ $(document).ready(function(){
 	});
 });
 
-$(document).ready(function(){
-	$('.client-slider').slick({
-		dots: false,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		prevArrow: '.prev_nav',
-		nextArrow: '.next_nav',
-		autoplay: true,
-		speed: 1000,
-		pauseOnHover: false,
-		pauseOnFocus: false,
-		responsive: [
-		{
-			breakpoint: 1025,
-			settings: {
-			slidesToShow: 1,
-       
-		}	
-	},
-	]	
-	});
-	
+//Counter Up
+jQuery(document).ready(function($) {
+   $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
 });
 
 //Animate on Scroll default settings
@@ -58,7 +43,6 @@ $(window).on('load', function () {
 
 $(function () {
 	AOS.init({
-  
 		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
 		offset: 200, // offset (in px) from the original trigger point
 		delay: 300, // values from 0 to 3000, with step 50ms
@@ -73,30 +57,3 @@ $(function () {
 
 //Preload all the images in the page
 imagesLoaded({background: true}, () => document.body.classList.remove('loading'));
-
-//Counter Up
-jQuery(document).ready(function($) {
-   $('.counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-});
-
-//MagnificPopup Video
-$('#play-video').magnificPopup({
-	disableOn: 300,
-	type: 'iframe',
-	mainClass: 'mfp-fade',
-	removalDelay: 300,
-	preloader: false,
-	fixedContentPos: false
-});
-
-//MagnificPopup Gallery
-$('.footer_grid').magnificPopup({
-	delegate: 'a',
-	type: 'image',
-	gallery: {
-		enabled: true
-	},
-});
