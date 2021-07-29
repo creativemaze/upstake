d08a0529ac60c1965cf02b552e386c9a805006e2
themewiz <61708713+themewiz@users.noreply.github.com>
2021-07-28 19:21:20 -0500

    update file

--- e-learning-main.js
+++ e-learning-main.js
@@ -7,12 +7,11 @@ $(document).ready(function(){
 		slidesToScroll: 1,
 		prevArrow: '.prev_slide',
 		nextArrow: '.next_slide',
-		autoplay: true,
+		autoplay: false,
 		speed: 1000,
 		pauseOnHover: false,
 		pauseOnFocus: false,
 		centerMode: true,
-		
 		responsive: [
 			{
 				breakpoint: 1025,
@@ -22,30 +21,22 @@ $(document).ready(function(){
 			},
 			
 			{
-				breakpoint: 600,
+				breakpoint: 601,
 				settings: {
 					slidesToShow: 1,
 					centerPadding: '0',
 				}		
 			},
-			
 		]
 	});
-	
 });
 
-$(document).ready(function(){
-	$('.client__slider').slick({	
-		dots: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: true,
-		autoplay: true,
-		speed: 1000,
-		pauseOnHover: false,
-		pauseOnFocus: false,
-	});
+//Counter Up
+jQuery(document).ready(function($) {
+   $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
 });
 
 //Animate on Scroll default settings
@@ -55,7 +46,6 @@ $(window).on('load', function () {
 
 $(function () {
 	AOS.init({
-  
 		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
 		offset: 200, // offset (in px) from the original trigger point
 		delay: 300, // values from 0 to 3000, with step 50ms
