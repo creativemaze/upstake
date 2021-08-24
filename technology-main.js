6c93690aad7d83c1db7fbc848381f422c1033022
themewiz <61708713+themewiz@users.noreply.github.com>
2021-08-24 10:53:05 -0500

    update file

--- technology-main.js
+++ technology-main.js
@@ -1,75 +1,43 @@
 "use strict";
 //Slick Slider (1) One
 $(document).ready(function(){
-	$('.tech_slider').slick({
+	$('.feature_slider').slick({
 		dots: false,
+		fade: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		arrows: false,
+		prevArrow: '.prev_button',
+		nextArrow: '.next_button',
 		autoplay: false,
-		fade: true,
-		adaptiveHeight: true,
 		speed: 1000,
 		pauseOnHover: false,
-		pauseOnFocus: false,		
-	});
-	
-	$('.tech_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-		$('.title_head a.active').removeClass('active');
-			$('.title_head a').eq(nextSlide).addClass('active');
-	});
-  
-	$('.title_head a').on('click', function(e){
-		e.preventDefault();
-		$('.title_head a.active').removeClass('active');
-		$(this).addClass('active');
-		var targetSlide = $(this).data('target');
-		$('.tech_slider').slick('slickGoTo', targetSlide );
+		pauseOnFocus: false,
+		adaptiveHeight: true,
 	});
 	
 });
 
 //Slick Slider (2) Two
 $(document).ready(function(){
-	$('.feature_slider').slick({
-		dots: false,
-		fade: true,
-		slidesToShow: 1,
+	$('.client-slider').slick({
+		dots: true,
+		slidesToShow: 2,
 		slidesToScroll: 1,
-		prevArrow: '.prev_slide',
-		nextArrow: '.next_slide',
+		arrows: false,
 		autoplay: false,
 		speed: 1000,
 		pauseOnHover: false,
 		pauseOnFocus: false,
-		adaptiveHeight: true,
-	});
-	
-});
-
-//Slick Slider (3) Three
-$(document).ready(function(){
-	$('.client-slider').slick({
-		dots: false,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		prevArrow: '.prev_nav',
-		nextArrow: '.next_nav',
-		autoplay: true,
-		speed: 1000,
-		pauseOnHover: false,
-		pauseOnFocus: false,
 		responsive: [
-		{
-			breakpoint: 1025,
-			settings: {
-			slidesToShow: 1,
+			{
+				breakpoint: 769,
+				settings: {
+					slidesToShow: 1,
        
-		}	
-	},
-	]	
+				}		
+			},
+		]	
 	});
-	
 });
 
 //F.A.Q.S Accordion
@@ -95,7 +63,6 @@ $(window).on('load', function () {
 
 $(function () {
 	AOS.init({
-  
 		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
 		offset: 200, // offset (in px) from the original trigger point
 		delay: 300, // values from 0 to 3000, with step 50ms
@@ -110,30 +77,3 @@ $(function () {
 
 //Preload all the images in the page
 imagesLoaded({background: true}, () => document.body.classList.remove('loading'));
-
-//Counter Up
-jQuery(document).ready(function($) {
-   $('.counter').counterUp({
-        delay: 10,
-        time: 2000
-    });
-});
-
-//MagnificPopup Video
-$('#play-video').magnificPopup({
-	disableOn: 300,
-	type: 'iframe',
-	mainClass: 'mfp-fade',
-	removalDelay: 300,
-	preloader: false,
-	fixedContentPos: false
-});
-
-//MagnificPopup Gallery
-$('.footer_grid').magnificPopup({
-	delegate: 'a',
-	type: 'image',
-	gallery: {
-		enabled: true
-	},
-});
