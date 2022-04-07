4ee421caeaca6178f40f4d1ec1d4428b698e21d6
creativemaze <61708713+creativemaze@users.noreply.github.com>
2022-04-07 15:45:38 -0500

    Add Files

---
+++ shop-main.js
@@ -0,0 +1,96 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.slickslide').slick({
+		autoplay: true,
+		infinite: true,
+		speed: 1000,
+		fade: true,
+		prevArrow: '.prev_button',
+		nextArrow: '.next_button',
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		dots: false,
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.client-slider').slick({
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: false,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: 
+				{
+					slidesToShow: 2,
+       
+				}	
+			},
+			
+			{
+				breakpoint: 501,
+				settings: 
+				{
+					slidesToShow: 1,
+       
+				}	
+			},
+		]	
+	});
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//MagnificPopup Gallery
+$('.footer_grid').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery: {
+		enabled: true
+	},
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
