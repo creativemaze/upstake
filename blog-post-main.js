4a1867dc98152d5840b6278e0a77a991d893fff1
creativemaze <61708713+creativemaze@users.noreply.github.com>
2022-04-19 18:56:33 -0500

    Add files

---
+++ blog-post-main.js
@@ -0,0 +1,48 @@
+"use strict";
+//MagnificPopup Lightbox Gallery
+$('.entry_image').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery:{
+		enabled: true
+	},
+});
+
+$( ".comment_reply" ).click(function() {
+	$('#form_area').animatescroll();
+});
+
+$( ".post_comments_count a" ).click(function() {
+	$('.blog_area_three').animatescroll();
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
