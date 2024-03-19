fbb238c349f657a6baf2b51c57290ca68035af07
creativemaze <61708713+creativemaze@users.noreply.github.com>
2024-03-19 14:45:34 -0500

    Update seo-agency-main.js

--- seo-agency-main.js
+++ seo-agency-main.js
@@ -1,79 +1,85 @@
 "use strict";
-//Slick Slider
+//Slick Slider (1) One
 $(document).ready(function(){
-	$('.client-slider').slick({
+	$('.feature_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.grid_slider').slick({
 		dots: false,
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		arrows: false,
-		autoplay: false,
+		prevArrow: '.prev_slide',
+		nextArrow: '.next_slide',
+		autoplay: true,
 		speed: 1000,
 		pauseOnHover: false,
 		pauseOnFocus: false,
-		dots: true,
+		centerMode: true,
 		responsive: [
 			{
-				breakpoint: 841,
+				breakpoint: 993,
 				settings: {
 					slidesToShow: 2,
-       
-				},
-			
+					
+				}			
 			},
 			
 			{
 				breakpoint: 601,
 				settings: {
 					slidesToShow: 1,
-       
-				},
-			
+					centerPadding: '100px',
+				}			
 			},
-		]	
+			
+			{
+				breakpoint: 430,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '35px',
+				}			
+			},
+			
+			{
+				breakpoint: 376,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '20px',
+				}			
+			},
+		]
 	});
-	
 });
 
-//Animated Progress Bar
-$(document).ready(function($) {
-  function animateElements() {
-	$('.progressbar').each(function() {
-		var elementPos = $(this).offset().top;
-		var topOfWindow = $(window).scrollTop();
-		var percent = $(this).find('.circle').attr('data-percent');
-		var percentage = parseInt(percent, 10) / parseInt(100, 10);
-		var animate = $(this).data('animate');
-		if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
-			$(this).data('animate', true);
-			$(this).find('.circle').circleProgress({
-			startAngle: -Math.PI / 2,
-			value: percent / 100,
-			thickness: 14,
-			size: 125,
-			animation: {
-				duration: 2800,
-			},
-			
-			fill: {
-				color: '#fcb045'
-			},
-			
-			emptyFill:{
-				color: '#111111'
-			}
-			
-			}).on('circle-animation-progress', function(event, progress, stepValue) {
-				$(this).find('div').text((stepValue * 100).toFixed(1) + "%");
-			}).stop();
-		}
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
     });
-}
-
-//Show animated elements
-animateElements();
-	$(window).scroll(animateElements);
 });
-
 
 //Animate on Scroll default settings
 $(window).on('load', function () {
