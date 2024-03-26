87950679e5871de45d94049ddd23076effa4600d
creativemaze <61708713+creativemaze@users.noreply.github.com>
2024-03-26 08:06:03 -0500

    Update digital-main.js

--- digital-main.js
+++ digital-main.js
@@ -1,10 +1,11 @@
 "use strict";
-//Slick Slider (1) One
+//Slick Slider
 $(document).ready(function(){
 	$('.digital_slider').slick({
 		dots: false,
 		arrows: false,
-		autoplay: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
 		fade: true,
 		speed: 1000,
 		pauseOnHover: false,
@@ -12,8 +13,8 @@ $(document).ready(function(){
 	});
 	
 	$('.digital_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-		$('.title_head a.active').removeClass('slick-current');
-			$('.title_head a').eq(nextSlide).addClass('slick-current');
+		$('.title_head a.active').removeClass('active');
+			$('.title_head a').eq(nextSlide).addClass('active');
 	});
   
 	$('.title_head a').on('click', function(e){
@@ -24,164 +25,6 @@ $(document).ready(function(){
 		$('.digital_slider').slick('slickGoTo', targetSlide );
 	});
 	
-});
-
-//Slick Slider (2) Two
-$(document).ready(function(){
-	$('.content_slider').slick({
-		dots: false,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		prevArrow: '.prev_button',
-		nextArrow: '.next_button',
-		autoplay: false,
-		speed: 1000,
-		pauseOnHover: false,
-		pauseOnFocus: false,
-		centerMode: true,
-		centerPadding: '20px',
-		responsive: [
-			{
-				breakpoint: 1025,
-				settings: {
-					slidesToShow: 2,
-				}		
-			},
-			
-			{
-				breakpoint: 601,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '75px',
-				}		
-			},
-			
-			{
-				breakpoint: 431,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '0',
-				}		
-			},
-			
-		]
-	});
-	
-});
-
-//Slick Slider (3) Three
-$(document).ready(function(){
-	$('.distribute_slider').slick({
-		dots: false,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		prevArrow: '.prev_button_2',
-		nextArrow: '.next_button_2',
-		autoplay: false,
-		speed: 1000,
-		pauseOnHover: false,
-		pauseOnFocus: false,
-		centerMode: true,
-		centerPadding: '20px',
-		responsive: [
-			{
-				breakpoint: 1025,
-				settings: {
-					slidesToShow: 2,
-				}		
-			},
-			
-			{
-				breakpoint: 601,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '75px',
-				}		
-			},
-			
-			{
-				breakpoint: 421,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '0',
-				}		
-			},
-			
-		]
-	});
-	
-});
-
-//Slick Slider (4) Four
-$(document).ready(function(){
-	$('.manage_slider').slick({
-		dots: false,
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		prevArrow: '.prev_button_3',
-		nextArrow: '.next_button_3',
-		autoplay: false,
-		speed: 1000,
-		pauseOnHover: false,
-		pauseOnFocus: false,
-		centerMode: true,
-		centerPadding: '20px',
-		responsive: [
-			{
-				breakpoint: 1025,
-				settings: {
-					slidesToShow: 2,
-				}		
-			},
-			
-			{
-				breakpoint: 601,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '75px',
-				}		
-			},
-			
-			{
-				breakpoint: 421,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '0',
-				}		
-			},
-		]
-	});
-	
-});
-
-//Slick Slider (5) Five
-$(document).ready(function(){
-	$('.client-slider').slick({
-		dots: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		arrows: false,
-		autoplay: false,
-		speed: 1000,
-		pauseOnHover: false,
-		responsive: [
-			{
-				breakpoint: 1025,
-				settings: {
-				slidesToShow: 2,
-       
-			}	
-		},
-		
-		{
-				breakpoint: 769,
-				settings: {
-				slidesToShow: 1,
-       
-			}	
-		},
-	]	
-	});
 });
 
 //MagnificPopup Video
